33bfe7b8222ba276001b27a24c69ea0b3a95ba14
Raphael <augusto.raphael@gmail.com>
2015-10-21 10:52:49 -0200

    alterar nome dos testes para descrever os cenarios de teste

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class SessaoTest {
 	
 	@Test
-	public void deveVender5ingressosSeHa10vagas() throws Exception {
+	public void deveVenderIngressosQuandoNumeroDeVagasMaiorQueRequerido() throws Exception {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(10);
 		
@@ -14,11 +14,19 @@ public class SessaoTest {
 	}
 
 	@Test
-	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
+	public void naoDeveVenderIngressosQuandoNumeroDeVagasMenorQueRequerido() throws Exception {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(2);
 
 		Assert.assertFalse(sessao.podeReservar(3));
+	}
+	
+	@Test
+	public void deveVenderIngressosQuandoNumeroDeVagasIgualRequerido() throws Exception {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(2);
+		
+		Assert.assertTrue(sessao.podeReservar(2));
 	}
 
 	@Test
@@ -28,14 +36,6 @@ public class SessaoTest {
 
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
-	}
-	
-	@Test
-	public void deveVender2ingressosSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-		sessao.setTotalIngressos(2);
-		
-		Assert.assertTrue(sessao.podeReservar(2));
 	}
 	
 }
