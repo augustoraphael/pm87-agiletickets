5eb6f7464219ad397f5fab7f1f7394d44e8d9053
Raphael <augusto.raphael@gmail.com>
2015-10-21 10:44:01 -0200

    removendo testes duplicados

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -4,21 +4,13 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SessaoTest {
-
-	@Test
-	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
-        sessao.setTotalIngressos(2);
-
-        Assert.assertTrue(sessao.podeReservar(1));
-	}
 	
 	@Test
 	public void deveVender5ingressosSeHa10vagas() throws Exception {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(10);
 		
-		Assert.assertTrue(sessao.podeReservar(1));
+		Assert.assertTrue(sessao.podeReservar(5));
 	}
 
 	@Test
